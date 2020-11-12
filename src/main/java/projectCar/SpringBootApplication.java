41faafc35a834475cb315f4dfcd3f138c3f089ba
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2020-11-12 23:00:22 +0300

    Spring Boot Application class config

---
+++ src/main/java/projectCar/SpringBootApplication.java
@@ -0,0 +1,72 @@
+package projectCar;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+    @Autowired
+    private Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+        System.out.println("DataSource: " + dataSource);
+        return dataSource;
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        Properties properties = new Properties();
+
+        properties.put("hibernate.show_sql",
+                environment.getProperty("spring.jpa.show-sql"));
+        properties.put("hibernate.hbm2ddl.auto",
+                environment.getProperty("spring.jpa.hibernate.ddl-auto"));
+        properties.put("hibernate.dialect",
+                environment.getProperty("spring.jpa.properties.hibernate.dialect"));
+        properties.put("current_session_context_class",
+                environment.getProperty("spring.jpa.properties.hibernate.current_session_context_class"));
+        properties.put("hibernate.search.default.directory_provider",
+                environment.getProperty("spring.jpa.properties.hibernate.search.default.directory_provider"));
+        properties.put("hibernate.search.default.indexBase",
+                environment.getProperty("spring.jpa.properties.hibernate.search.default.indexBase"));
+
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        factoryBean.setPackagesToScan("projectCar.entity");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.afterPropertiesSet();
+
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("SessionFactory: " + sf);
+        return sf;
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+        return transactionManager;
+    }
+}
--- src/main/java/projectCar/entity/Parameter.java
+++ src/main/java/projectCar/entity/Parameter.java
@@ -1,9 +1,10 @@
 package projectCar.entity;
 
-import javax.persistence.*;
-import java.sql.Date;
-import java.util.Objects;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Data
 @Entity
 @Table(name = "parameters", schema = "projectcar")
 public class Parameter {
@@ -44,131 +45,4 @@ public class Parameter {
     @JoinColumn(name = "FK_cars")
     private Car car;
 
-    public Parameter() {
-    }
-
-    public Parameter(String mark, String model, int year, int firstMileage, int mass, String color, double averageRate,
-                     String vin, String registrationNumber, Car car) {
-        this.mark = mark;
-        this.model = model;
-        this.year = year;
-        this.firstMileage = firstMileage;
-        this.mass = mass;
-        this.color = color;
-        this.averageRate = averageRate;
-        this.vin = vin;
-        this.registrationNumber = registrationNumber;
-        this.car = car;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMark() {
-        return mark;
-    }
-
-    public void setMark(String mark) {
-        this.mark = mark;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getFirstMileage() {
-        return firstMileage;
-    }
-
-    public void setFirstMileage(int mileage) {
-        this.firstMileage = mileage;
-    }
-
-    public int getMass() {
-        return mass;
-    }
-
-    public void setMass(int mass) {
-        this.mass = mass;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double getAverageRate() {
-        return averageRate;
-    }
-
-    public void setAverageRate(double averageRate) {
-        this.averageRate = averageRate;
-    }
-
-    public String getVin() {
-        return vin;
-    }
-
-    public void setVin(String vin) {
-        this.vin = vin;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public Car getCar() {
-        return car;
-    }
-
-    public void setCar(Car car) {
-        this.car = car;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Parameter parameter = (Parameter) o;
-        return id == parameter.id &&
-                firstMileage == parameter.firstMileage &&
-                mass == parameter.mass &&
-                Double.compare(parameter.averageRate, averageRate) == 0 &&
-                Objects.equals(mark, parameter.mark) &&
-                Objects.equals(model, parameter.model) &&
-                Objects.equals(year, parameter.year) &&
-                Objects.equals(color, parameter.color) &&
-                Objects.equals(vin, parameter.vin) &&
-                Objects.equals(registrationNumber, parameter.registrationNumber) &&
-                Objects.equals(car, parameter.car);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, mark, model, year, firstMileage, mass, color, averageRate, vin, registrationNumber, car);
-    }
 }
