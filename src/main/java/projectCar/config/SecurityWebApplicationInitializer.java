0d9bf02b70e332f74e51128b9b3182515b40ff0c
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2020-12-07 21:20:02 +0300

    correct background

--- src/main/java/projectCar/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package projectCar.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
